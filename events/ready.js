592648e3200e601b42c8e95e69c8cbe9d123772c
Glitch (sideways-order) <none>
2019-11-05 08:54:15 +0000

    🐽🍿 Checkpoint ./events/ready.js:7473276/57

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
 }, 8000);
 client.user.setPresence({
         game: {
-            name: `${prefix}js Online`,
+            name: `${prefix}js | ${prefix}html | Aktif`,
             type: 'WATCHING'
         },
         status: 'online'
