ace673c6786580d3b6c1ba8d8aae39c599e69b7a
Glitch (sideways-order) <none>
2019-11-16 19:05:40 +0000

    🕰👨‍❤️‍💋‍👨 Checkpoint ./komutlar/ıhlamur.js:7473276/6746

--- komutlar/ıhlamur.js
+++ komutlar/ıhlamur.js
@@ -1,26 +1,27 @@
 const Discord = require("discord.js");
 const ayarlar = require("../ayarlar.json");
+//
 
 exports.run = (client, message, params) => {
   if (!message.guild) {
     const ozelmesajuyari = new Discord.RichEmbed()
-      .setColor(0xff0000)
+      .setColor("RANDOM")
       .setTimestamp()
       .setAuthor(message.author.username, message.author.avatarURL)
-      .addField("Eğlence Komutları Özel Mesajlarda Kullanılamaz!");
+      .addField("**Eğlence Komutları Özel Mesajlarda Kullanılamaz!**");
     return message.author.sendEmbed(ozelmesajuyari);
   }
   if (message.channel.type !== "dm") {
     const ıhlamur = new Discord.RichEmbed()
-      .setAuthor(message.author.username + " Ihlamur İçti!")
-      .setColor("#3447003")
+      .setTitle("<@" + message.author.id + "> Ihlamur İçti")
+      .setColor("RANDOM")
       .setTimestamp()
       .setDescription("")
       .setURL("")
       .setImage(
-        `tps://cdn.discordapp.com/attachments/645234211808346112/645290712962826240/hangi-hastaliga-hangi-kis-icecegi-iyi-gelir--2641463.Gif.gif`
+        `https://cdn.discordapp.com/attachments/634056817017880586/645334668857704478/hangi-hastaliga-hangi-kis-icecegi-iyi-gelir--2641463.Gif.gif`
       );
-    return message.channel.send(ıhlamur);
+    return message.channel.sendEmbed(ıhlamur);
   }
 };
 
