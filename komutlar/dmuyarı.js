ec8d45bc83631902767b0d473da87afdd204509d
Glitch (sideways-order) <none>
2019-11-16 21:50:46 +0000

    🏃🍰 Checkpoint ./komutlar/dmuyarı.js:7473276/2469 ./sayaç.json:7473276/145 ./package.json:7473276/4005

--- komutlar/dmuyarı.js
+++ komutlar/dmuyarı.js
@@ -1,30 +1,49 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js')
+const os = require('os')
+var speedTest = require('speedtest-net');
 
-module.exports.run = async (bot, message, args) => {
-  //if(message.author.id !== "534724915593609216") return;
-  //message.delete();
-  let botmessage = args.join(" ");
-  let embed = new Discord.RichEmbed()
-    .setTitle(`Link Kısaltıcı`)
-    .setDescription(
-      `[Kısaltılan Link İçin Tıkla]( https://www.pnd.tl/st/?api=9d073993d7f2b7a84f17aa51a3f68e0333f90cc1&url=${botmessage})`
-    )
-    .setColor("BLUE")
-    .setFooter(
-      "Uyarı: Kısaltılan Linklerdeki Reklamlardan Botumuz Sorumlu Değildir!"
-    );
-  message.channel.sendEmbed(embed);
-};
+exports.run = (client, message) => {
+  var osType = os.type();
+
+     message.channel.send(`:timer: | İnternet Hızınız Yükleniyor, lütfen bekleyin.`).then(m => m.delete(3000));
+     message.channel.send(`:timer: | Pinginiz Hesaplanıyor, lütfen bekleyin.`).then(m => m.delete(9000));
+     message.channel.send(`:timer: | Host Aranıyor, lütfen bekleyin.`).then(m => m.delete(9000));
+     message.channel.send(`:timer: | İşletim Sistemi Hesaplanıyor, lütfen bekleyin.`).then(m => m.delete(12000));
+  
+  if (osType === 'Darwin') osType = 'macOS'
+  else if (osType === 'Windows') osType = 'Windows'
+  else if (osType === 'Linux') osType = 'Linux'
+  else if (osType === 'Ubuntu') osType = 'Ubuntu'
+  else osType = os.type();
+    var test = speedTest({maxTime: 5000});
+    test.on('data', data => {
+const embed = new Discord.RichEmbed()
+ .setColor('#000000')
+.setTitle('**İnternet Bilgilerin**')
+.addField(`:arrow_down: İndirme: **${data.speeds.download} MB**      :arrow_up: Yükleme: **${data.speeds.upload} MB **`)
+.addField(` :round_pushpin: Ping: **${client.ping}**`)
+.addField(`:level_slider: İşletim Sistemi: **${osType}** \n \n:desktop: İnternet Sağlayıcısı: **${data.client.isp}** \n \n:file_cabinet: Host: **${data.server.host}**`)
+
+  message.channel.send(embed)
+});
+ 
+    test.on('error', err => {
+  console.log(err);
+});
+}
+
+
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["hız"],
   permLevel: 0,
-  kategori: ``
+  kategori: "sahip"
 };
 
 exports.help = {
-  name: `link-kısalt`,
-  description: `Link kısaltır!`,
-  usage: `link-kısalt {link} `
+  name: 'hız-test',
+  description: 'internet hız testi yapar!',
+  usage: 'hız'
 };
+ 
