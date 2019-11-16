a621bf4e1f3898c8dc8173f91ac66e5c7b55162c
Glitch (sideways-order) <none>
2019-11-16 18:30:40 +0000

    🎁🚈 Checkpoint ./komutlar/ıhlamur.js:7473276/1052

---
+++ komutlar/ıhlamur.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+exports.run = (client, message, params) => {
+  if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+      .setColor(0xff0000)
+      .setTimestamp()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .addField("Eğlence Komutları Özel Mesajlarda Kullanılamaz!");
+    return message.author.sendEmbed(ozelmesajuyari);
+  }
+  if (message.channel.type !== "dm") {
+    const ıhlamur = new Discord.RichEmbed()
+      .setAuthor(message.author.username + " Ihlamur İçti!")
+      .setColor("#3447003")
+      .setTimestamp()
+      .setDescription("")
+      .setImage(
+        "https://cdn.discordapp.com/attachments/645234211808346112/645290712962826240/hangi-hastaliga-hangi-kis-icecegi-iyi-gelir--2641463.Gif.gif"
+      );
+    return message.channel.sendE(ıhlamur);
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["ıhlamur-iç", "ıhlamuriç"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "ıhlamur",
+  description: "Ihlamur İçersiniz",
+  usage: "ıhlamur"
+};
