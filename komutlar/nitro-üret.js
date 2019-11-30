9d5e0a5c78cfa8dc92a8c4b32cb4cb80a26d7fe0
Glitch (sideways-order) <none>
2019-11-30 12:27:57 +0000

    🐕💕 Checkpoint ./komutlar/nitro-üret.js:7473276/1980

--- komutlar/nitro-üret.js
+++ komutlar/nitro-üret.js
@@ -1,42 +1,34 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
   function rasteleSembol(uzunluk, semboller) {
+    var maske = "";
 
-var maske = '';
+    if (semboller.indexOf("a") > -1) maske += "abcdefghijklmnopqrstuvwxyz";
 
-if (semboller.indexOf('a') > -1) maske += 'abcdefghijklmnopqrstuvwxyz';
+    if (semboller.indexOf("A") > -1) maske += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-if (semboller.indexOf('A') > -1) maske += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (semboller.indexOf("0") > -1) maske += "0123456789";
 
-if (semboller.indexOf('0') > -1) maske += '0123456789';
+    var sonuc = "";
 
+    for (var i = uzunluk; i > 0; --i) {
+      sonuc += maske[Math.floor(Math.random() * maske.length)];
+    }
 
-var sonuc = '';
+    return sonuc;
+  }
 
- 
-
-for (var i = uzunluk; i > 0; --i) 
-
-{
-
-sonuc += maske[Math.floor(Math.random() * maske.length)];
-
-}
-
-return sonuc;
-
-}
-
-function spamla(){
-    message.channel.send("https:/"+"/discord.gift/" + rasteleSembol(16,'0aA'));
-}
-        var veri = message.content.toString();
-        var veriSn = veri.substring(10)
-        var saniye = parseInt(veriSn);
-        var saniye1 = saniye * 1000;
-        setInterval(spamla, saniye1);
-        
+  function spamla() {
+    message.channel.send(
+      "https:/" + "/discord.gift/" + rasteleSembol(24, "0aA")
+    );
+  }
+  var veri = message.content.toString();
+  var veriSn = veri.substring(10);
+  var saniye = parseInt(veriSn);
+  var saniye1 = saniye * 1000;
+  setInterval(spamla, saniye1);
 };
 
 exports.conf = {
@@ -46,7 +38,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'nitroüret',
-  description: 'Botun pingini gösterir.',
-  usage: 'ping'
+  name: "üret",
+  description: "Botun pingini gösterir.",
+  usage: "ping"
 };
