a9fc8272018391d129d67f4f1ced9a3e3311f2d3
Glitch (sideways-order) <none>
2020-02-26 21:56:12 +0000

    📺🛰 Checkpoint ./util/eventLoader.js:7473276/1829 ./events/guildMemberAdd.js:7473276/2 ./events/guildCreate.js:7473276/6 ./events/guildMemberRemove.js:7473276/4

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,6 +1,9 @@
-const reqEvent = (event) => require(`../events/${event}`);
+const reqEvent = event => require(`../events/${event}`);
 module.exports = client => {
-  client.on('ready', () => reqEvent('ready')(client));
-  client.on('message', reqEvent('message'));
-  client.on('guildMemberUpdate', reqEvent('guildMemberUpdate'));
-}; 
+  client.on("ready", () => reqEvent("ready")(client));
+  client.on("message", reqEvent("message"));
+  client.on("guildMemberUpdate", reqEvent("guildMemberUpdate"));
+  client.on("guildMemberAdd", reqEvent("guildMemberAdd"));
+  client.on("guildMemberRemove", reqEvent("guildMemberRemove"));
+  client.on("guildCreate", reqEvent("guildCreate"));
+};
