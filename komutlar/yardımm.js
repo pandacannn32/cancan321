049218931280e26efff1b83d648687b824eebe99
Glitch (bos-altyapii) <none>
2020-10-29 21:29:00 +0000

    ☎️🦄 Checkpoint ./komutlar/emoji-bilgi.js:17825458/803 ./komutlar/yardımm.js:17825458/1035

--- komutlar/yardımm.js
+++ komutlar/yardımm.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js');
 exports.run = function(client, message) {
 const embed = new Discord.RichEmbed()
-.setColor('Yellow')
-.setTitle('Chwas Gif Komutları')
+.setColor('RANDOM')
+.setTitle('Maiar Gif Komutları')
 .setTimestamp()
-.addField('<a:750076062716788807:756912478902026326>        |  .animal ','Rastgele hayvan gifleri atar')
-.addField('<a:750076071721828452:756912488112455762>        |  .anime  ','Rastgele anime gifleri atar ')
-.addField('<:pxel_kalp:756912655519711312>        |  .baby ','Rastgele bebek gifleri atar')
-.addField('<a:ats:756912744116125788>        |  .couple  ', 'Rastgele sevgili gifleri atar')
-.addField('<a:750076057679429656:756912470970335262>        |  .man   ','Rast gele erkek gifleri atar')
-.addField('<a:714606187261198450:756917847627661420>        |  .woman  ', 'Rastgele kadın gifleri atar')
-.addField('<a:TAC:756912721059905587>        |  .yapımcım  ', 'Yapımcıları görürsün  ')
-.addField('<a:yesl:756917598976999474>      |  .gif   ','Özel anime gifleri atar')
-.addField('<a:gk:756917851776090263>      |  .anime   ','Rastgele anime gifleri atar')
+.addField('.animal ','Rastgele hayvan gifleri atar')
+.addField('.anime  ','Rastgele anime gifleri atar ')
+.addField('.baby ','Rastgele bebek gifleri atar')
+.addField('.couple  ', 'Rastgele sevgili gifleri atar')
+.addField('.man   ','Rast gele erkek gifleri atar')
+.addField('.woman  ', 'Rastgele kadın gifleri atar')
+.addField('.yapımcım  ', 'Yapımcıları görürsün  ')
+.addField('.gif   ','Özel anime gifleri atar')
+.addField('.anime   ','Rastgele anime gifleri atar')
 
 
 
@@ -25,9 +25,9 @@ const embed = new Discord.RichEmbed()
 
 
 
-.setFooter(`Bu komut ${message.author.username} tarafından istendi!`, message.author.avatarURL)
-.addField(`» Linkler`, `[Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=756892207776006236&permissions=2048&scope=bot) **|** `)
-        .setFooter(`Chwas | Bot Sürümü : 0.5.1 Beta | Varsayılan Prefix : .`, client.user.avatarURL)
+.setFooter(`Bu komut ${message.author.username} Tarafından İstendi!`, message.author.avatarURL)
+.addField(`» Linkler`, `[Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=771482580553564181&permissions=8&scope=bot) **|** `)
+
 
 
 
